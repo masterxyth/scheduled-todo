ab774c06c882b06dcf44feec4d6c5ae31e461e40
Rizwan Qaiser <rizwan@Rizwans-MacBook-Pro.local>
2017-06-26 01:27:25 -0700

    create_user working with local db w/hash

--- my_app/blog/views.py
+++ my_app/blog/views.py
@@ -3,7 +3,11 @@ from flask import render_template, request
 
 from my_app import app
 from my_app.blog.models import TODOS
+from my_app.blog.models import PasswordHelper
+from my_app.blog.models import DBHelper
 
+DB = DBHelper()
+PH = PasswordHelper()
 
 
 @app.route('/')
@@ -18,10 +22,16 @@ def home():
 def signup():
     return render_template('signup.html')
 
-@app.route('/register')
+@app.route('/register', methods=['POST'])
 def register():
     email = request.form.get("Email")
     password = request.form.get("Password")
+    password = str(password).encode('utf-8')
+    salt = PH.get_salt()
+    salt = str(salt).encode('utf-8')
+    hashed = PH.get_hash(password + salt)
+    DB.create_user(email, salt, hashed)
+    return render_template('test.html', email = email)
 
 
 @app.route('/add')
