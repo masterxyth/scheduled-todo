11595e6d9c6973508f18bfa9167ef3a84c79346a
Rizwan Qaiser <rizwan@Rizwans-MacBook-Pro.local>
2017-06-25 23:17:58 -0700

    sign up page and landing page html files

--- my_app/blog/models.py
+++ my_app/blog/models.py
@@ -1,3 +1,13 @@
+import base64
+import hashlib
+import os
+
+import pymongo
+
+
+DATABASE = "schedtodo"
+
+
 TODOS = {
 
 '8:00AM - 9:00AM':'',
@@ -17,3 +27,24 @@ TODOS = {
 '10:00PM - 11:00PM':''
 
 }
+
+
+class PasswordHelper:
+
+    def get_hash(self,plain):
+        return hashlib.sha512(plain).hexdigest()
+
+    def get_salt(self):
+        return base64.b64encode(os.urandom(20))
+
+    def validate_password(self, plain, salt, expected):
+        return self.get_hash(plain + salt) == expected
+
+class DBHelper():
+
+    def __init__(self):
+        client = pymongo.MongoClient()
+        self.db = client[DATABASE]
+
+    def create_user(self, email, salt, hashed):
+        self.db.users.insert({"email": email, "salt": salt, "hashed": hashed})
--- my_app/blog/views.py
+++ my_app/blog/views.py
@@ -1,14 +1,28 @@
+import pymongo
 from flask import render_template, request
+
 from my_app import app
 from my_app.blog.models import TODOS
 
 
 
 @app.route('/')
+def index():
+    return render_template('index.html')
 @app.route('/home')
 
 def home():
     return render_template('home.html', todos=TODOS)
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/register')
+def register():
+    email = request.form.get("Email")
+    password = request.form.get("Password")
+
 
 @app.route('/add')
 def add():
